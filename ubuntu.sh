c7ec73db54caf42b22f3f01c996ae259bb01350a
Murat Mirgün ERCAN <45563173+MuratSs@users.noreply.github.com>
2020-11-14 15:00:45 +0300

    ubuntu setup script upload

---
+++ ubuntu.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+
+sudo apt-get upgrade 
+
+sudo apt install git
+
+sudo apt install docker.io  
+
+sudo apt install kubectl
+
+sudo apt install neofetch   
+
+sudo apt install curl
+
+sudo apt install golang-go 
+
+sudo apt install openjdk-11-jdk
+
+sudo apt install gimp 
+
+sudo apt install npm 
